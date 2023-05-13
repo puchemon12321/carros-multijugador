07b9dad287e5347da3c9ed377a87771d571b2ed0
puchemon12321 <118579913+puchemon12321@users.noreply.github.com>
2023-05-12 19:53:52 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,15 +10,15 @@ var obstacle1,obstacle1Image,obstacle2,obstacle2Image;
 //BP
 function preload() {
   backgroundImage = loadImage("assets/background.png");
-  car1_img = loadImage("./assets/car1.png");
-  car2_img = loadImage("./assets/car2.png");
-  track = loadImage("./assets/track.jpg");
-  fuelImage = loadImage ("./assets/fuel.png");
-  powerCoinImage = loadImage ("./assets/goldCoin.png");
+  car1_img = loadImage("assets/car1.png");
+  car2_img = loadImage("assets/car2.png");
+  track = loadImage("assets/track.jpg");
+  fuelImage = loadImage ("assets/fuel.png");
+  powerCoinImage = loadImage ("assets/goldCoin.png");
   lifeImage=loadImage("assets/life.png");
-  obstacle1Image =loadImage("./assets/obstacle1.png");
-  obstacle2Image =loadImage("./assets/obstacle2.png");
-  blastImage=loadImage("./assets/blast.png");
+  obstacle1Image =loadImage("assets/obstacle1.png");
+  obstacle2Image =loadImage("assets/obstacle2.png");
+  blastImage=loadImage("assets/blast.png");
 }
 
 //BP
